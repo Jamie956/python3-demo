003de9ea67197538c05590cce50deb6b25f2d8e8
Jamie956 <752492509@qq.com>
2021-11-02 10:16:09 +0800

    替换excel非法字符

--- esToExcel.py
+++ esToExcel.py
@@ -3,6 +3,7 @@ from elasticsearch import Elasticsearch
 from openpyxl import Workbook
 import os
 from datetime import datetime
+import re
 
 index = 'test_index'
 doc = '_doc'
@@ -11,11 +12,13 @@ es = Elasticsearch()
 
 wb = Workbook()
 ws = wb.active
+ILLEGAL_CHARACTERS_RE = re.compile(r'[\000-\010]|[\013-\014]|[\016-\037]')
+start = datetime.now()
 
 # 创建输出目录
 output_dir = './output'
 ouputExist = os.path.exists(output_dir)
-if bool(1-ouputExist):
+if bool(1 - ouputExist):
     print('文件夹不存在, 创建输出目录 ./output')
     os.mkdir(output_dir)
 
@@ -32,8 +35,7 @@ for key in mapping:
 ws.append(fields)
 
 # 查询首页，获取返回滚动id
-total_spend_time = 0
-start = datetime.now()
+
 page = es.search(body=search_body, index=index, doc_type=doc, scroll="2m")
 # 滚动id
 sid = page['_scroll_id']
@@ -42,11 +44,14 @@ total = page['hits']['total']
 scroll_size = page['hits']['total']
 
 # 写到excel
-for i,hit in enumerate(page['hits']['hits']):
+for i, hit in enumerate(page['hits']['hits']):
     write_row = []
-    for j,field in enumerate(fields):
+    for j, field in enumerate(fields):
         if field in hit["_source"].keys():
-            write_row.append(str(hit["_source"][field]))
+            field_value = str(hit["_source"][field])
+            # 去除excel非法字符，否在抛出异常 openpyxl.utils.exceptions.IllegalCharacterError
+            field_value = ILLEGAL_CHARACTERS_RE.sub(r'', field_value)
+            write_row.append(field_value)
         else:
             # 字段不存在，写入空
             write_row.append('')
@@ -55,24 +60,26 @@ for i,hit in enumerate(page['hits']['hits']):
 # 保存页数据到excel
 wb.save(output_dir + "/esData.xlsx")
 
-spend_time = (datetime.now()-start).microseconds
-total_spend_time += spend_time
-print('首页查询',str(len(page['hits']['hits'])),'条 保存到excel','耗时（秒）',spend_time/1000000)
 
-#滚动写到excel
+print('首页查询', str(len(page['hits']['hits'])), '条 保存到excel', '耗时（秒）', (datetime.now() - start).microseconds / 1000000)
+
+# 滚动写到excel
 cur_size = len(page['hits']['hits'])
-while(scroll_size > 0):
-    start = datetime.now()
+while (scroll_size > 0):
+    scroll_start = datetime.now()
     page = es.scroll(scroll_id=sid, scroll='2m')
     sid = page['_scroll_id']
     # 命中数据条数
     scroll_size = len(page['hits']['hits'])
     # 写到excel
-    for i,hit in enumerate(page['hits']['hits']):
+    for i, hit in enumerate(page['hits']['hits']):
         write_row = []
-        for j,field in enumerate(fields):
+        for j, field in enumerate(fields):
             if field in hit["_source"].keys():
-                write_row.append(str(hit["_source"][field]))
+                field_value = str(hit["_source"][field])
+                # 去除excel非法字符，否在抛出异常 openpyxl.utils.exceptions.IllegalCharacterError
+                field_value = ILLEGAL_CHARACTERS_RE.sub(r'', field_value)
+                write_row.append(field_value)
             else:
                 # 字段不存在，写入空
                 write_row.append('')
@@ -81,8 +88,6 @@ while(scroll_size > 0):
     # 保存页数据到excel
     wb.save(output_dir + "/esData.xlsx")
     cur_size += scroll_size
-    spend_time = (datetime.now()-start).microseconds
-    total_spend_time += spend_time
-    print('滚动查询',str(cur_size), '/', str(total) ,'条 保存到excel','耗时（秒）',spend_time/1000000)
+    print('滚动查询', str(cur_size), '/', str(total), '条 保存到excel', '耗时（秒）', (datetime.now() - scroll_start).microseconds / 1000000)
 
-print('总耗时（秒）',total_spend_time/1000000)
+print('总耗时（秒）', (datetime.now() - start).microseconds / 1000000)
